d296f131971a87d2312365ecd985b54f4cdb7860
Aiden Baker <aidenbak9@gmail.com>
2024-11-12 09:44:31 -0600

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Aiden Baker on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tic Tac ToeTests/Tic_Tac_ToeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Tic_Tac_ToeTests.swift
+//  Tic Tac ToeTests
+//
+//  Created by Aiden Baker on 11/12/24.
+//
+
+import Testing
+
+struct Tic_Tac_ToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
