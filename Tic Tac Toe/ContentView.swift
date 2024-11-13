186c4c9a214381d39696263206507d7858ce873e
Aiden Baker <aidenbak9@gmail.com>
2024-11-13 11:49:49 -0600

    fix tictactoe title

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
+                .font(.title)
+                .bold()
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3), content: {
                 ForEach(0..<9) { index in
                     ZStack {
